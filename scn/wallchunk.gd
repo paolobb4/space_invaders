2f5b39c9717b130f4eb8a97c54d08a438698050d
Paolo Barresi <paolobarresi@bbfactory.it>
2019-08-11 20:23:35 +0200

    Add wall regeneration

--- scn/wallchunk.gd
+++ scn/wallchunk.gd
@@ -1,7 +1,14 @@
 extends StaticBody
 
+
+var dead = false
+
+
+func _ready():
+    $"AnimationPlayer".play("spawn")
+
 func hit():
-    queue_free()
+    $"..".disable_wall_chunk(self)
 
 func _on_enemy_detector_body_entered(body):
     hit()
