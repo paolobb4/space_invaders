2b7c3ada8588b51ac69b555bbf09d7ec1e31e66d
Paolo Barresi <paolobarresi@bbfactory.it>
2019-09-27 22:25:32 +0200

    Add enemies folder, update enemie timers min/max options, add enemies hp

--- project/scn/enemies/enemy.gd
+++ project/scn/enemies/enemy.gd
@@ -9,13 +9,16 @@ export var weapon_cooldown_max = 5.0
 export var random_shoot_min = 2.0
 export var random_shoot_max = 10.0
 export var shoot_probability = 0.5
+export var hit_points = 1
 
 
 func _ready():
-    $"Timer_weapon_cooldown".wait_time = rand_range(weapon_cooldown_min, weapon_cooldown_max)
-    $"Timer_weapon_cooldown".start()
-    $"Timer_rand_shoot".wait_time = rand_range(random_shoot_min, random_shoot_max)
-    $"Timer_rand_shoot".start()
+    if (weapon_cooldown_min and weapon_cooldown_max):
+        $"Timer_weapon_cooldown".wait_time = rand_range(weapon_cooldown_min, weapon_cooldown_max)
+        $"Timer_weapon_cooldown".start()
+    if (random_shoot_min and random_shoot_max):
+        $"Timer_rand_shoot".wait_time = rand_range(random_shoot_min, random_shoot_max)
+        $"Timer_rand_shoot".start()
 
 
 func _process(delta):
@@ -36,10 +39,12 @@ func _on_Timer_rand_shoot_timeout():
 
 func shoot():
     if not $"RayCast_front".is_colliding():
-        $"Timer_weapon_cooldown".wait_time = rand_range(weapon_cooldown_min, weapon_cooldown_max)
-        $"Timer_weapon_cooldown".start()
-        $"Timer_rand_shoot".wait_time = rand_range(random_shoot_min, random_shoot_max)
-        $"Timer_rand_shoot".start()
+        if (weapon_cooldown_min and weapon_cooldown_max):
+            $"Timer_weapon_cooldown".wait_time = rand_range(weapon_cooldown_min, weapon_cooldown_max)
+            $"Timer_weapon_cooldown".start()
+        if (random_shoot_min and random_shoot_max):
+            $"Timer_rand_shoot".wait_time = rand_range(random_shoot_min, random_shoot_max)
+            $"Timer_rand_shoot".start()
 
         var w = choice([
             Blast, Blast, Blast, Blast, Blast, Blast, Blast, Blast, Blast,
@@ -54,7 +59,9 @@ func shoot():
 
 
 func hit():
-    queue_free()
+    hit_points -= 1
+    if hit_points == 0:
+        queue_free()
 
 
 func _on_tree_exiting():
